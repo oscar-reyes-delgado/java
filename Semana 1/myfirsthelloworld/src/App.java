755b51634db52982ea47b62082423b819ef6c948
Oscar Reyes <gab.rey.del@gmail.com>
2026-01-14 00:32:19 -0600

    lol

--- Semana 1/myfirsthelloworld/src/App.java
+++ Semana 1/myfirsthelloworld/src/App.java
@@ -6,5 +6,20 @@ public class App {
         System.out.print("Escribe tu nombre aqui bro: ");
         String nombre = sc.nextLine();
         System.out.println("Que buen nombre tienes "+nombre);
+        int edad = 0;
+        edad = edad(sc, edad);
+        System.out.println("Bro estas bien viejo, "+ edad + "??");
     }
+
+    public static int edad(Scanner scanner, int edad){
+        System.out.println("A continuacion vas a ingresar tu edad,");
+        System.out.println("no se va a completar hasta que ingreses");
+        System.out.println("un numero valido.");
+        do{
+            System.out.print("Escribe tu edad: ");
+            edad = scanner.nextInt();}
+        while(edad >0 & edad <100);
+        return edad;
+    }
+
 }
