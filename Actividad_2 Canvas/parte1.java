59bf6ae175960e6d4d818d914a6ee812b85ae33a
Oscar Reyes <gab.rey.del@gmail.com>
2026-01-23 13:51:10 -0600

    Parte 1 Actividad 2

--- Actividad_2 Canvas/parte1.java
+++ Actividad_2 Canvas/parte1.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class parte1 {
-    
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("=======================================\nSistema de acceso a oficina de Director\n=======================================");
+        System.out.print("Escribe tu nombre: ");
+        String nombre = lector.nextLine();
+        String director = "Homero Gutierrez";
+        lector.close();
+
+        if (nombre.equals(director)){
+            System.out.println("Director, puede ingresar a la oficina.\n");
+        }else{
+            System.out.println("Acceso denegado\n");
+        }
+
+    }
 }
