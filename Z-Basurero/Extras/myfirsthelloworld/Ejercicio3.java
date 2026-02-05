e5f179c313277b4b46c432803f0770f5685a6115
Oscar Reyes <gab.rey.del@gmail.com>
2026-02-04 22:25:29 -0600

    ojo

--- Z-Basurero/Extras/myfirsthelloworld/Ejercicio3.java
+++ Z-Basurero/Extras/myfirsthelloworld/Ejercicio3.java
@@ -5,13 +5,50 @@ public class Ejercicio3 {
         int opcion;
         do{
             System.out.println("=============================");
-            System.out.println("     Mini calculadora");
+            System.out.println("      Mini calculadora");
             System.out.println("=============================");
             System.out.println("1. Sumar");
             System.out.println("2. Restar");
             System.out.println("3. Salir");
             System.out.println("=============================");
             System.out.print("Ingresa la opcion: ");
-            int opcion = teclado.nextInt();
+            opcion = teclado.nextInt();
+
+            switch(opcion){
+                case 1 -> {
+                    double suma = sumar(teclado);
+                    System.out.println("\nSuma: "+ suma + "\n");
+                }
+                case 2 -> {
+                    double resta = restar(teclado);
+                    System.out.println("\nResta: "+ resta + "\n");
+                }
+                case 3 -> {
+                    System.out.println("\nSaliendo de la calculadora\n");
+                }
+            }
+        }while(opcion != 3);
+    }
+
+    public static double sumar(Scanner lector){
+
+        System.out.print("\nEscribe el primer numero: ");
+        double primero = lector.nextDouble();
+
+        System.out.print("\nEscribe el segundo numero: ");
+        double segundo = lector.nextDouble();
         
+        return primero + segundo;
+    }
+
+    public static double restar(Scanner lector){
+
+        System.out.print("\nEscribe el primer numero: ");
+        double primero = lector.nextDouble();
+
+        System.out.print("\nEscribe el segundo numero: ");
+        double segundo = lector.nextDouble();
+        
+        return primero - segundo;
+    }
 }
